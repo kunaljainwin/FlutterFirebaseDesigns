43e0b15305e86345724945f5a148bc4ac70af09a
Kunal jain <72144717+kunaljainwin@users.noreply.github.com>
2022-11-25 19:17:28 +0530

    Create terms_of_service.dart

---
+++ terms_of_service.dart
@@ -0,0 +1,15 @@
+Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          "Privacy policy",
+                          textScaleFactor: 0.9,
+                          style: TextStyle(color: Colors.black54),
+                        ),
+                        Text(
+                          "Terms of Service",
+                          textScaleFactor: 0.9,
+                          style: TextStyle(color: Colors.black54),
+                        ),
+                      ],
+                    ),
